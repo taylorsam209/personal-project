e10b1c7e4d031c1a5be42f24bdb496ebfd7aa5c4
Taylor Sam <taylorsam209@yahoo.com>
2017-10-20 20:54:30 -0600

    Added styling for search button

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -23,17 +23,15 @@ class Landing extends Component {
     const { getListings } = this.props;
     return (
       <div className="Landing">
-        <Nav header='Welcome' />
+        <Nav header='Welcome' style="margin-bottom: 60px" />
         <div className="background-img-container">
+          <div className="search-container">
           <input className="input-bar" type="text" value={this.state.input} onChange={(e) => { this.setState({ input: e.target.value }) }}  placeholder="Find Vegan & Vegetarian near..." />
-          
           <Link to='/listing' className="submit-btn" onClick={() => { getListings(this.state.input) }}>Submit</Link>
+          </div>
           <div className="description-container">
-            <h3>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            <h3> The Green Fork was created to allow you to locate vegeterian and vegan friendly resources within the community.
+              We took the hardwork out of your hands and let you focus on having a variety of choices. Give it a try, begin your search!
                 </h3>
           </div>
         </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 import "./Nav.css";
 import { getCurrentUser } from '../../ducks/reducer';
 import { connect } from 'react-redux';
+import logo from "../../assets/greenfork.jpg";
+import vegan from "../../assets/vegan.png";
 
 class Nav extends Component {
 
     handleLogin() {
         if (!this.props.user) {
-            return <a href={process.env.REACT_APP_LOGIN}><button>Login</button></a>
+            return <a className="no-underline" href={process.env.REACT_APP_LOGIN}><div className="btn">Login</div></a>
         } else
             return (
                 <div className="login-container">
-                    <Link to="/profile" ><button>Profile</button></Link>
-                    <a href={'http://localhost:3010/auth/logout'}><button>Logout</button></a>
+                    <Link className="no-underline" to="/profile"><div className="btn">Profile</div></Link>
+                    <a className="no-underline" href={'http://localhost:3010/auth/logout'}><div className="btn">Logout</div></a>
                 </div>
             )
     }
@@ -22,16 +24,19 @@ class Nav extends Component {
         let { header } = this.props;
 
         return (
-            <div className="header-parent">
-                <div className="header-container">
-                    <div className="header-child-left">
-                        <Link to="/" className="header-logo" >The Green Fork</Link>
-                    </div>
-                    <div className="header-child-mid">
-                        <span className="header-word">{header}</span>
-                    </div>
-                    <div className="header-child-right">
-                        {this.handleLogin.bind(this)()}
+            <div>
+                <img className="logo" src={logo} alt="fork" />
+                <div className="header-parent">
+                    <div className="header-container">
+                        <div className="header-child-left">
+                            <Link to="/" className="header-logo" >The Green Fork</Link>
+                        </div>
+                        <div className="header-child-mid">
+                            <span className="header-word">{header}</span>
+                        </div>
+                        <div className="header-child-right">
+                            {this.handleLogin.bind(this)()}
+                        </div>
                     </div>
                 </div>
             </div>
